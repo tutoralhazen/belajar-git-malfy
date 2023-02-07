60ef5d965d6a0d8c137f609fec3b5b573e3a62ed
Malfy <malfy.boor@gmail.com>
2023-02-07 20:54:09 +0700

    Menambahkan function multiplication dan division

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ def addition(a,b):
 def substraction(a,b):
 	result = a - b
 	print(a,"-",b,"=",result)
+
+def multiplication(a,b):
+	result = a * b
+	print(a,"*",b,"=",result)
+
+def division(a,b):
+	result = a / b
+	print(a,"/",b,"=",result)
